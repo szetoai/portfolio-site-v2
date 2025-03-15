5458ac83429c90f106d625f3ac2925583b6cbacd
szetoai <aidan.szeto99910@gmail.com>
2025-03-15 18:45:23 -0400

    projects background

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -2,11 +2,14 @@ import React from "react";
 
 function Projects() {
     return (
-        <ul>
+        <>
+            <h1 className="sectionHead">Featured Projects</h1>
+            <ul>
             <li>
                 <img src="https://i.ibb.co/NdKwQmY1/2025-03-1423-18-05-ezgif-com-video-to-gif-converter.gif" alt="qbert"/>
             </li>
-        </ul>
+            </ul>
+        </>
     );
 }
 
