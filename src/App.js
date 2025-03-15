a1aaf48d0dcf4cbcf50dcb90999e2b9dc97daeee
szetoai <aidan.szeto99910@gmail.com>
2025-03-14 23:23:40 -0400

    beginning to add projects (qbert gif added)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Navbar from './navbar/navbar';
-
+import Projects from './projects/projects';
 
 function App() {
   return (
     <div className="App">
+      {/* Navigation Bar */}
       <Navbar />
       <img className='jeff' src='https://i.ibb.co/kV0rW4YK/jeff.png' alt='jeff' />
-      <div className='Intro'>
+      {/* The introduction the user sees */}
+      <div className='intro'>
         <img id='selfie' src='https://i.ibb.co/7xqHrMW9/senior-photo-modified.png' alt='selfie' />
         <h1>Welcome to my website (v2)!</h1>
         <p id='desc'>My last one kept getting flagged for misleading content :/</p>
+      </div>
+      {/* Projects Section */}
+      <div className='projects'>
+        <Projects />
       </div>
     </div>
   );
---
+++ src/projects/projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Projects() {
+    return (
+        <ul>
+            <li>
+                <img src="https://s8.ezgif.com/tmp/ezgif-848be86b38f34a.gif" alt="qbert"/>
+            </li>
+        </ul>
+    );
+}
+
+export default Projects;
