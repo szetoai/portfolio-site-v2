1034fbaf9c5d14f2ed31a2b95ed60998b46bebf3
szetoai <aidan.szeto99910@gmail.com>
2025-03-20 16:18:22 -0400

    made images consistent size and started to make on hover for projects

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import "./projects.css";
 
 function Projects() {
+    const [p1, setP1] = useState("project");
+
     return (
         <>
             <h1 id="projectsHead">Featured Projects</h1>
             <div id="projectsList">
-                <div className="project">
-                    <img className="projectImg" id="projectImg1" src="https://i.ibb.co/2145NRRg/qbert.gif" alt="qbert"/>
+                <div className={p1} id="project1" onMouseOver={() => setP1("activeProject")} onMouseLeave={() => setP1("project")}>
+                    <img className="projectImg" id="projectImg1" src="https://i.ibb.co/FqKBRCHz/qbert.gif" alt="qbert"/>
                     <h2>MyQbert</h2>
                 </div>
                 <span />
-                <div className="project">
-                    <img className="projectImg" id="projectImg2" src="https://i.ibb.co/qMjQTNFn/face-recog.gif" alt="face recognition"/>
+                <div className="project" id="project2">
+                    <img className="projectImg" id="projectImg2" src="https://i.ibb.co/zVCTqfF0/face-recog.gif" alt="face recognition"/>
                     <h2>AI Facial Recognition</h2>
                 </div>
             </div>
